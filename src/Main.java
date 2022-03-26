21cf5ae61901dda27401a88ed5078b4145fedea3
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-03-25 23:52:21 -0500

    start making boss fight

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,18 @@ import World.*;
 
 public class Main {
     public static void main(String[] args) {
- Hero alex = new Hero("alex", "barbarian", listOfCommonWeapons.commonWeapons()[1]);
-Enemy enemy = new Enemy("minotaur", "barbarian", 1, listOfCommonWeapons.commonWeapons()[1]);
+        Hero alex = new Hero("alex", "barbarian", listOfCommonWeapons.commonWeapons()[1]);
+        Boss fighter = new Boss("malchuar", "vampire", 1, listOfCommonWeapons.commonWeapons()[1]);
+
+        new fight(alex, fighter);
+ /*Enemy enemy = new Enemy("minotaur", "barbarian", 1, listOfCommonWeapons.commonWeapons()[1]);
+
 alex.setCurrHP(10);
 new fight(alex, enemy);
 
         System.out.println("this should show up after the fight. ");
         new fight(alex, new Enemy("wolf", "assassin",1,listOfCommonWeapons.commonWeapons()[1]));
+    }
+*/
     }
 }
