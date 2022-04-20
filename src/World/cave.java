2fe4195f493834f75dc6db529d945546c8b94046
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-19 21:47:42 -0500

    need to figure out the name of the final boss. also want to restart with new knowledge of servlets. maybe mmake this an html game. if i learn how to make it transfer out

--- src/World/cave.java
+++ src/World/cave.java
@@ -3,7 +3,7 @@ package World;
 public class cave implements worldbuilding {
 
     public String describeArea() {
-        return "it's cold..\n";
+        return "i check the map. ive entered the ghastly woods. with my sword in hand and a patch of 5 potions. i begin my mission. \n return with the head of the Werewolf {name}\n";
     }
 
 
